8adfd5dd91947993222b88ac77119dda44e55fbb
Swarna7414 <swarnasaisankar044@gmail.com>
2025-10-31 16:25:08 -0500

    added the yaml file

--- src/main/java/com/ServiceOne/TheOne/TheOneApplication.java
+++ src/main/java/com/ServiceOne/TheOne/TheOneApplication.java
@@ -3,9 +3,11 @@ package com.ServiceOne.TheOne;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class TheOneApplication {
 
 	public static void main(String[] args) {
